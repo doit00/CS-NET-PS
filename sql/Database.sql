055b60d6e0ad643c21492e870dd7d3ae1220d05b
unknown <doit00@github.com>
2018-07-11 14:40:46 +0200

    db script added

---
+++ sql/Database.sql
@@ -0,0 +1,28 @@
+﻿-- SqlLocalDB.exe c MSSQLLocalDB
+-- SqlLocalDB.exe s MSSQLLocalDB
+-- SqlLocalDB.exe i MSSQLLocalDB
+
+create database demo
+go
+
+use demo
+go
+
+Create table Logs
+(
+id int identity(1,1) PRIMARY KEY
+,message nvarchar(255)
+,date datetime2
+)
+go
+Insert Logs(message, date)
+values ('error',SYSDATETIME())
+go
+Insert Logs(message, date)
+values 	('warning',SYSDATETIME())
+go
+Insert Logs(message, date)
+values ('debug',SYSDATETIME())
+
+select * from Logs
+
