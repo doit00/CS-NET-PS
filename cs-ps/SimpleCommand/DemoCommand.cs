1a04c0ab5008ba968470e9809ed99fe3cfd58d84
unknown <doit00@github.com>
2018-07-11 13:32:24 +0200

    initial commit

---
+++ cs-ps/SimpleCommand/DemoCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCommand
+{
+    [Cmdlet(VerbsCommon.Get,"DateTimeInfo")]
+    public class DemoCommand: Cmdlet
+    {
+        [Parameter(Mandatory =false,ParameterSetName ="FormatSet",ValueFromPipeline =true)]
+        public string Format { get; set; }
+        protected override void ProcessRecord()
+        {
+            if (Format != null && Format.Length > 0)
+            {
+                WriteObject(DateTime.Today.ToString(Format));
+            }
+            else
+            {
+                WriteObject(DateTime.Today);
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Get,"Max")]
+    public class MaxCommand : Cmdlet
+    {
+        List<int> ints = new List<int>();
+
+        [Parameter(ValueFromPipeline =true)]
+        public int Number { get; set; }
+        protected override void ProcessRecord()
+        {
+            ints.Add(Number);
+        }
+
+        protected override void EndProcessing()
+        {
+            WriteObject(ints.Max());
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Get, "Sum")]
+    public class SumCommand : Cmdlet
+    {
+        int sum = 0;
+
+        [Parameter(ValueFromPipeline = true)]
+        public int Number { get; set; }
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                checked
+                {
+                    sum += Number;
+                }
+            }
+            catch (OverflowException oe)
+            {
+                WriteError(new ErrorRecord(
+                    oe, "int32 overflow exception", ErrorCategory.InvalidOperation, Number));
+                
+            }
+            catch(Exception se)
+            {
+                WriteError(new ErrorRecord(
+                    se, "System Exception", ErrorCategory.InvalidOperation, Number));
+            }
+           
+        }
+
+        protected override void EndProcessing()
+        {
+            WriteObject(sum);
+        }
+    }
+}
---
+++ cs-ps/SimpleCommand/EncryptionCommands.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCommand
+{
+    class EncryptionCommands
+    {
+        public static void DecryptData(string password, string saltValue, string sourcePath, string destinationPath)
+        {
+            var encryptedSource = File.ReadAllBytes(sourcePath);
+
+            AesManaged alg;
+            byte[] key, iv;
+            InitAlgorithm(password, saltValue, out alg, out key, out iv);
+            var encryptor = alg.CreateDecryptor(key, iv);
+            var target = new MemoryStream();
+            var source = encryptedSource;
+            var cs = new CryptoStream(target, encryptor, CryptoStreamMode.Write);
+            cs.Write(source, 0, source.Length);
+            cs.FlushFinalBlock();
+            var encrypted = target.ToArray();
+            var output = Encoding.Unicode.GetString(encrypted);
+
+            File.WriteAllText(destinationPath, output);
+        }
+
+        public static void EncryptData(string password, string saltValue, string sourcePath, string destinationPath)
+        {
+            AesManaged alg;
+            byte[] key, iv;
+            InitAlgorithm(password, saltValue, out alg, out key, out iv);
+
+            var encryptor = alg.CreateEncryptor(key, iv);
+            var target = new MemoryStream();
+            var source = Encoding.Unicode.GetBytes(File.ReadAllText(sourcePath));
+            var cs = new CryptoStream(target, encryptor, CryptoStreamMode.Write);
+            cs.Write(source, 0, source.Length);
+            cs.FlushFinalBlock();
+            var encrypted = target.ToArray();
+          
+            File.WriteAllBytes(destinationPath, encrypted);
+        }
+
+        private static void InitAlgorithm(string password, string saltValue, out AesManaged alg, out byte[] key, out byte[] iv)
+        {
+            alg = new AesManaged();
+            var salt = Encoding.Unicode.GetBytes(saltValue);
+            var hash = new Rfc2898DeriveBytes(password, salt);
+            key = hash.GetBytes(alg.KeySize / 8);
+            iv = hash.GetBytes(alg.BlockSize / 8);
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Set,"EncryptedFile")]
+    public class EncryptCommand:Cmdlet
+    {
+        [Parameter]
+        public string Password { get; set; }
+        [Parameter]
+        public string Salt { get; set; }
+        [Parameter]
+        public string SourcePath { get; set; }
+        [Parameter]
+        public string DestinationPath { get; set; }
+
+        protected override void EndProcessing()
+        {
+            EncryptionCommands.EncryptData(Password, Salt, SourcePath, DestinationPath);
+            WriteObject("Encryption completed");
+
+        }
+
+    }
+
+    [Cmdlet(VerbsCommon.Get, "DecryptedFile")]
+    public class DecryptCommand : Cmdlet
+    {
+        [Parameter]
+        public string Password { get; set; }
+        [Parameter]
+        public string Salt { get; set; }
+        [Parameter]
+        public string SourcePath { get; set; }
+        [Parameter]
+        public string DestinationPath { get; set; }
+
+        protected override void EndProcessing()
+        {
+            EncryptionCommands.DecryptData(Password, Salt, SourcePath, DestinationPath);
+            WriteObject("Decryption completed");
+
+        }
+
+    }
+
+
+}
